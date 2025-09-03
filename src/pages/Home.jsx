3b1a4364e15cbba6086f7af4b6a346e7489ad8c4
AdolfoRV <adolfo.rojas.v@ug.uchile.cl>
2025-09-03 01:47:40 -0400

    feat(pages): add Home, NotFound, and Notes pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import navbarData from "@/data/navegation/home-navbar.json";
+import { Navbar } from "@/components/Navbar";
+import { StarBackground } from "@/components/background/StarBackground";
+import { PresentationSection } from "@/components/home/PresentationSection";
+import { AboutSection } from "@/components/home/about/AboutSection";
+import { CurriculumSection } from "@/components/home/curriculum/CurriculumSection";
+import { ProjectsSection } from "@/components/home/projects/ProjectsSection";
+import { Footer } from "@/components/Footer";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">      
+      <StarBackground />
+      <Navbar items={navbarData} />
+      {/* Main Content */}
+      <main>
+        <PresentationSection />
+        <AboutSection />
+        <CurriculumSection />
+        <ProjectsSection />
+      </main>
+      
+      <Footer />
+    </div>
+  );
+};
